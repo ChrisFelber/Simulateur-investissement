0a10a112c6f1caae7996c76152483e4e47ec6ab3
ChrisFelber <cfelber.bap@gmail.com>
2026-08-29 20:45:50 +0200

    Add investment simulation configuration

--- js/config.js
+++ js/config.js
@@ -1,1 +1,21 @@
-// Configuration globale de la V2.
+window.InvestmentConfig = Object.freeze({
+  defaults: Object.freeze({
+    initialCapital: 5000,
+    contribution: 100,
+    contributionFrequency: 'monthly',
+    annualReturn: 0.035,
+    durationYears: 10
+  }),
+
+  frequencies: Object.freeze({
+    monthly: Object.freeze({ periodsPerYear: 12 }),
+    weekly: Object.freeze({ periodsPerYear: 52 })
+  }),
+
+  strategies: Object.freeze({
+    income: Object.freeze({ annualReturn: 0.035 }),
+    balanced: Object.freeze({ annualReturn: 0.055 }),
+    growth: Object.freeze({ annualReturn: 0.075 }),
+    equities: Object.freeze({ annualReturn: 0.095 })
+  })
+});
